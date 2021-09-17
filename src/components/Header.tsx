cb5eb3d578abf5c9a483b8ea9c2ec84394c6faca
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2021-09-17 16:46:40 -0300

    updating logo and fixing some problems with image reference

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-import '../styles/header.scss'
+import "../styles/header.scss";
 
 export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <h1 className="logo">
+          <strong>to</strong>.do
+        </h1>
       </div>
     </header>
-  )
+  );
 }
